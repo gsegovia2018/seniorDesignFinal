f3b524d425d93112a63fc11823f635d8f4b1eae9
Guillermo <gsegovia2018@my.fit.edu>
2019-10-26 19:10:57 -0400

    Registration and Login done

--- DriveAndShare/SeniorDesign/ViewController/SignUpViewController.swift
+++ DriveAndShare/SeniorDesign/ViewController/SignUpViewController.swift
@@ -12,24 +12,18 @@ import Firebase
 
 class SignUpViewController: UIViewController {
 
-    
+    //Get all the elements
+    @IBOutlet weak var backButton: UIButton!
+    @IBOutlet weak var doneButton: UIButton!
     @IBOutlet weak var firstNameTextField: UITextField!
-    
-    
     @IBOutlet weak var lastNameTextField: UITextField!
-    
-    
     @IBOutlet weak var emailTextField: UITextField!
-    
-    
     @IBOutlet weak var passwordTextField: UITextField!
-    
-    
+    @IBOutlet weak var repeatPasswordTextField: UITextField!
+    @IBOutlet weak var yearBirthTextField: UITextField!
+    @IBOutlet weak var genderTextField: UITextField!
     @IBOutlet weak var signUpButton: UIButton!
-    
-    
     @IBOutlet weak var errorLabel: UILabel!
-    
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -44,10 +38,14 @@ class SignUpViewController: UIViewController {
         errorLabel.alpha = 0
         
         //Style Elements
+        
         Utilities.styleTextField(firstNameTextField)
         Utilities.styleTextField(lastNameTextField)
         Utilities.styleTextField(emailTextField)
         Utilities.styleTextField(passwordTextField)
+        Utilities.styleTextField(repeatPasswordTextField)
+        Utilities.styleTextField(yearBirthTextField)
+        Utilities.styleTextField(genderTextField)
         Utilities.styleFilledButton(signUpButton)
     }
     
@@ -57,7 +55,10 @@ class SignUpViewController: UIViewController {
         if firstNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
             lastNameTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
             emailTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
-            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" {
+            passwordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            repeatPasswordTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            yearBirthTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == "" ||
+            genderTextField.text?.trimmingCharacters(in: .whitespacesAndNewlines) == ""{
             return "Please fill in all fields"
         }
         
@@ -68,7 +69,18 @@ class SignUpViewController: UIViewController {
             //Password isnt secure enough
             return "Please make sure your pasword is at least 8 characters, contains a special character and a number."
         }
-        
+        //Check if the passwords are the same
+        if passwordTextField.text != repeatPasswordTextField.text {
+            //Passwords are not the same
+            return "Make sure your passwords are the same"
+        }
+        //Check if the gender is correct
+        let genderOption: [String] = ["Man", "man", "Woman", "woman"]
+        let genderText = genderTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+        if !genderOption.contains(genderText)  {
+            return "Make sure you type 'Man or Woman'"
+        }
+    
         return nil
         
     }
@@ -121,6 +133,55 @@ class SignUpViewController: UIViewController {
             }
         }
     }
+    
+    @IBAction func doneButtonTapped(_ sender: Any) {
+        
+        //Validate the fields
+        let error = validateFields()
+        
+        if error != nil {
+            
+            //There's something wrong with the fields, show error message
+            showError(error!)
+        }
+        else{
+            
+            //Create cleaned versions of the data
+            let firstName = firstNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let lastName = lastNameTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let email = emailTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            let password = passwordTextField.text!.trimmingCharacters(in: .whitespacesAndNewlines)
+            
+            
+            //Create the user
+            Auth.auth().createUser(withEmail: email, password: password) { (result, err) in
+                
+                //Check for errors
+                if err != nil {
+                    
+                    //There was an error creating the user
+                    self.showError("Error creating user")
+                }
+                else {
+                
+                    //User was created successfully, now store the first name and last name
+                    let db = Firestore.firestore()
+                    
+                    db.collection("users").addDocument(data: ["firstName":firstName, "lastName":lastName, "uid":result!.user.uid]) { (error) in
+                        
+                        if error != nil {
+                            //Show error message
+                            self.showError("Error saving user data")
+                        }
+                    }
+                    
+                    //Transition to the home screen
+                    self.transitionToHome()
+                }
+            }
+        }
+    }
+    
     
     
     //------------------FUNCTIONS------------------------------------------------
